24333425bce9fb07839402b31a8d62f823079647
Jeremy Lindblom <jeremeamia@gmail.com>
2010-08-09 07:25:27 -0700

    Changed sample ACL rules file to use new rule declaration format.

--- acl.php
+++ acl.php
@@ -1,13 +1,6 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-// Allow all to access the userguide
-ACL::add_rule(ACL::rule()
-	->for_controller('userguide')
-	->allow_all()
-);
-
 // Allow all to access the welcome controller
-ACL::add_rule(ACL::rule()
+ACL::rule()
 	->for_controller('welcome')
-	->allow_all()
-);
+	->allow_all();
