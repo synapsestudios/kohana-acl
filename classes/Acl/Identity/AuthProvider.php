aa87c31fcde411bb3c966dbaecd4c3dddaf8ec3b
Ben Youngblood <ben@synapsestudios.com>
2013-06-07 09:46:54 -0700

    use interfaced classes to get roles or just return authenticated/default roles

--- classes/Acl/Identity/AuthProvider.php
+++ classes/Acl/Identity/AuthProvider.php
@@ -1,8 +1,13 @@
 <?php
 
+/**
+ * Assumes that your user model returned from Kohana Auth has a get_role() method
+ */
 class AuthProvider implements Acl_Identity_ProviderInterface {
 
 	protected $_default_role = 'guest';
+
+	protected $_authenticated_role = 'authenticated';
 
 	public function __construct(Kohana_Auth $auth)
 	{
@@ -11,9 +16,14 @@ class AuthProvider implements Acl_Identity_ProviderInterface {
 
 	public function get_identity_roles()
 	{
-		if ($this->_auth->logged_in())
-			return array($this->_auth->get_user()->get_role());
+		if ( ! $this->_auth->logged_in())
+			return array($this->_default_role);
 
-		return $this->_default_role;
+		$user = $this->_auth->get_user();
+
+		if ($user instanceof Acl_User_RoleInterface)
+			return $this->_auth->get_user()->get_roles();
+
+		return array($this->_authenticated_role);
 	}
 }
